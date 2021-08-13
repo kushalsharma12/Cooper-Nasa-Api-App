b418a32aa6f47858c9b7342803a0088525687ce6
kushalsharma12 <uie19bme1081@gmail.com>
2021-08-13 09:42:23 +0530

    small update

--- app/src/main/java/com/kushalsharma/nasawalli/interface.kt
+++ app/src/main/java/com/kushalsharma/nasawalli/interface.kt
@@ -7,20 +7,20 @@ import retrofit2.http.GET
 
 
 const val BASE_URL = "https://api.nasa.gov/"
-const val API_KEY = "AAZq7v3WHXOKIRC0MGeTLSkSbOyK4YE7xk3wbefH"
+const val API_KEY = BuildConfig.NASA_API_KEY
 
 interface NasaInterface {
 
     @GET("planetary/apod?api_key=$API_KEY")
     fun getNasainfo(): Call<Nasa>
 
-    @GET("techtransfer/patent/?telescope&api_key=$API_KEY")
+    @GET("techtransfer/patent/?telescope&api_key=${API_KEY}")
     fun getNasaPatents(): Call<Patent>
 
 }
-//    https://api.nasa.gov/planetary/apod?api_key=AAZq7v3WHXOKIRC0MGeTLSkSbOyK4YE7xk3wbefH
+//    https://api.nasa.gov/planetary/apod?api_key=${API_KEY}
 
-//    https://api.nasa.gov/techtransfer/patent/?engine&api_key=AAZq7v3WHXOKIRC0MGeTLSkSbOyK4YE7xk3wbefH
+//    https://api.nasa.gov/techtransfer/patent/?engine&api_key=${API_KEY}
 
 object NasaService {
     val nasaInterface: NasaInterface
